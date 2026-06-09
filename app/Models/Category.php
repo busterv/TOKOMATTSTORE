214157a8121b56aed7615d71573569b43f2eb86c
Bustersv <riyhanputrarohman@gmail.com>
2026-06-09 08:31:32 +0700

    Initial commit via AutoRepo

---
+++ app/Models/Category.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+class Category extends Model {
+    protected $fillable = ['category_name'];
+    public function menus() { return $this->hasMany(Menu::class); }
+}
