e2a7675094044eeee73c241a8d044b61e0065cba
Bustersv <riyhanputrarohman@gmail.com>
2026-06-09 08:33:02 +0700

    Initial commit via AutoRepo

---
+++ database/migrations/2026_06_07_085751_create_transactions_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+   public function up(): void {
+    Schema::create('transactions', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('user_id')->constrained()->onDelete('cascade');
+        $table->string('transaction_code')->unique();
+        $table->decimal('total_price', 15, 2);
+        $table->decimal('total_paid', 15, 2);
+        $table->decimal('total_return', 15, 2);
+        $table->dateTime('transaction_date');
+        $table->timestamps();
+    });
+}
+public function down(): void { Schema::dropIfExists('transactions'); }
+};
