53f59c5f3511b4b39b9dfcdf0466f6aa4a44691c
Bustersv <riyhanputrarohman@gmail.com>
2026-06-09 08:31:35 +0700

    Initial commit via AutoRepo

---
+++ app/Models/Menu.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Menu extends Model
+{
+    protected $table = 'menu'; // ← WAJIB ADA INI
+    
+    protected $fillable = ['category_id', 'item_name', 'brand', 'price', 'stock', 'description'];
+    protected $casts = ['price' => 'decimal:2'];
+    
+    public function category() { return $this->belongsTo(Category::class); }
+    public function details() { return $this->hasMany(TransactionDetail::class); }
+}
